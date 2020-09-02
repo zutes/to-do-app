34096401d3f2a26ac376d1b0527c819ebf92cad0
zutes <zutes3479@gmail.com>
2020-09-02 16:39:40 -0600

    Updated vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <md-card-header>
+      <div class="title">To-Do List</div>
+    </md-card-header>
+    <md-card>
+      <md-field>
+        <md-input v-model="currentTodo" @keydown.enter="addTodo()" placeholder=" Add a to-do here" />
+      </md-field>
+      <ul class="todos">
+        <li v-for="todo in todos" :key="todo.id">
+          <md-input type="text" v-model="todo.label" placeholder="update todo">
+            <span
+              v-if="todo.editing === false"
+              class="editing"
+              :class="{ editing: todo == editedTodo }"
+              @dblclick="editTodo(todo)"
+            >{{ todo.label }}</span>
+          </md-input>
+          <input type="checkbox" @change="completeTodo(todo)" :checked="todo.isComplete" />
+          <md-button class="md-raised" @click="removeTodo(todo)">Delete To-Do</md-button>
+        </li>
+      </ul>
+    </md-card>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      todos: [],
+      currentTodo: "",
+      completed: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false,
+        editing: false,
+      });
+      this.currentTodo = "";
+    },
+    completeTodo(todo) {
+      todo.isComplete = !todo.isComplete;
+    },
+    editTodo(todo) {
+      const index = this.todos.indexOf(todo);
+      this.todos[index].editing = !this.todos[index].editing;
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  background-color: #d3d3d3;
+}
+.title {
+  font-size: 42px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: #2a6259;
+}
+
+.md-raised {
+  font-size: 20px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-left: 5px;
+  color: red;
+}
+
+.md-card {
+  width: 100%;
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+}
+
+li {
+  display: flex;
+  padding-top: 20px;
+  padding-bottom: 20px;
 }
 </style>
