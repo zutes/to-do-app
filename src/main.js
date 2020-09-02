e6633a36d4ad6b04f234a0d988ee0ecc292c96cc
zutes <zutes3479@gmail.com>
2020-09-02 16:39:47 -0600

    Updated js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import {
+  MdField,
+  MdCard,
+  MdButton,
+  MdCheckbox,
+} from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
 
-Vue.config.productionTip = false
+Vue.use(MdField);
+Vue.use(MdCard);
+Vue.use(MdButton);
+Vue.use(MdCheckbox);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
